a006f732550d84da22b8dbb3298161a064bf9212
Raghuraj Pratap Singh <Raghurajpratap2810@gmail.com>
2025-10-04 18:20:39 +0530

    Fix NODE_ENV and DATABASE_URL configuration for production deployment

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -18,7 +18,7 @@ const config = {
     logging: false
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
